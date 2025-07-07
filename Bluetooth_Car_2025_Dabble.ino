d6e41afee66e850f797bf845f884b3b90ddd5e66
Shelley Nguyen <scn220006@utdallas.edu>
2025-07-07 14:40:48 -0500

    Update Bluetooth_Car_2025_Dabble.ino
    Code for Bluetooth Car using the App Dabble. Dabble has its own library.

--- Bluetooth_Car_2025_Dabble.ino
+++ Bluetooth_Car_2025_Dabble.ino
@@ -2,90 +2,101 @@
 #define INCLUDE_GAMEPAD_MODULE
 #include <Dabble.h>
 
-//Right motor
-int enableRightMotor=5; 
-int rightMotorPin1=7;
-int rightMotorPin2=8;
+// Right motor direction pins
+int rightMotorPin1 = 7;
+int rightMotorPin2 = 8;
 
-//Left motor
-int enableLeftMotor=6;
-int leftMotorPin1=9;
-int leftMotorPin2=10;
+// Left motor direction pins
+int leftMotorPin1 = 9;
+int leftMotorPin2 = 10;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  pinMode(enableRightMotor,OUTPUT);
-  pinMode(rightMotorPin1,OUTPUT);
-  pinMode(rightMotorPin2,OUTPUT);
+  pinMode(rightMotorPin1, OUTPUT);
+  pinMode(rightMotorPin2, OUTPUT);
   
-  pinMode(enableLeftMotor,OUTPUT);
-  pinMode(leftMotorPin1,OUTPUT);
-  pinMode(leftMotorPin2,OUTPUT);
+  pinMode(leftMotorPin1, OUTPUT);
+  pinMode(leftMotorPin2, OUTPUT);
 
-  rotateMotor(0,0);   
-    
-  Dabble.begin(9600, 2, 3);
+  stopMotors();
+
+  Dabble.begin(9600, 2, 3);  // RX = 2, TX = 3
 }
-
-
 
 void loop()
 {
-  int rightMotorSpeed=0;
-  int leftMotorSpeed=0;
   Dabble.processInput();
+
   if (GamePad.isUpPressed())
   {
-    rightMotorSpeed = 255;
-    leftMotorSpeed = 255;
+    moveForward();
   }
-
-  if (GamePad.isDownPressed())
+  else if (GamePad.isDownPressed())
   {
-    rightMotorSpeed = -255;
-    leftMotorSpeed = -255;
+    moveBackward();
   }
-
-  if (GamePad.isLeftPressed())
+  else if (GamePad.isLeftPressed())
   {
-    rightMotorSpeed = 255;
-    leftMotorSpeed = 0;
+    turnLeft();
   }
-
-  if (GamePad.isRightPressed())
+  else if (GamePad.isRightPressed())
   {
-    rightMotorSpeed = 0;
-    leftMotorSpeed = 255;
+    turnRight();
   }
-
-  rotateMotor(rightMotorSpeed, leftMotorSpeed);
+  else
+  {
+    stopMotors();
+  }
 }
 
-void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
+// function to move forward
+void moveForward()
 {
-  if (rightMotorSpeed < 0)
-  {
-    digitalWrite(rightMotorPin1,LOW);
-    digitalWrite(rightMotorPin2,HIGH);    
-  }
-  else if (rightMotorSpeed >= 0)
-  {
-    digitalWrite(rightMotorPin1,HIGH);
-    digitalWrite(rightMotorPin2,LOW);      
-  }
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
 
-  if (leftMotorSpeed < 0)
-  {
-    digitalWrite(leftMotorPin1,LOW);
-    digitalWrite(leftMotorPin2,HIGH);    
-  }
-  else if (leftMotorSpeed >= 0)
-  {
-    digitalWrite(leftMotorPin1,HIGH);
-    digitalWrite(leftMotorPin2,LOW);      
-  }
-
-  analogWrite(enableRightMotor, abs(rightMotorSpeed));
-  analogWrite(enableLeftMotor, abs(leftMotorSpeed));    
+  digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
 }
+
+// function to move backward
+void moveBackward()
+{
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, HIGH);
+
+  digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, HIGH);
+}
+
+// function to turn left
+void turnLeft()
+{
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+
+  digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, LOW);  // left motor off
+}
+
+// function to turn right
+void turnRight()
+{
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, LOW);  // right motor off
+
+  digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
+}
+
+// stop both motors
+void stopMotors()
+{
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, LOW);
+
+  digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, LOW);
+}
+}
+"}
